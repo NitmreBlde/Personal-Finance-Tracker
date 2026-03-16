b43010b36b4fecf2553a7d3a81cecf373ce919f6
Nireeksha K R <nishuniree135@gmail.com>
2026-03-16 18:14:12 +0530

    Update chart data fetching and configuration

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
-const ctx = document.getElementById('expenseChart');
+const ctx = document.getElementById('expenseChart').getContext('2d');
+
+fetch('/chart-data')
+.then(response => response.json())
+.then(data => {
 
 new Chart(ctx, {
+
 type: 'pie',
+
 data: {
-labels: ['Food','Transport','Bills','Shopping'],
+labels: ['Income', 'Expense'],
+
 datasets: [{
-data: [40,20,25,15],
+data: [data.income, data.expense],
+
 backgroundColor:[
-'#FF8A8A',
-'#7C8CF8',
 '#6ED6A0',
-'#BFA6FF'
-]
+'#FF8A8A'
+],
+
+borderWidth:1
 }]
+},
+
+options:{
+responsive:true,
+plugins:{
+legend:{
+position:'bottom'
 }
+}
+}
+
+});
+
 });
